3be184a868010a18f628622bffcbdf6c81bc80f7
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-05-25 17:42:05 +0530

    queries for categories

--- e-commerce-app/index.js
+++ e-commerce-app/index.js
@@ -84,13 +84,31 @@ const products = [
   },
 ];
 
+const categories = [
+  {
+    id: "c01b1ff4-f894-4ef2-b27a-22aacc2fca70",
+    name: "Kitchen",
+  },
+  {
+    id: "34115aac-0ff5-4859-8f43-10e8db23602b",
+    name: "Garden",
+  },
+  {
+    id: "d914aec0-25b2-4103-9ed8-225d39018d1d",
+    name: "Sports",
+  },
+];
+
 const typeDefs = gql`
   type Query {
     hello: String
     products: [Product!]!
     product(id: ID!): Product
+    categories: [Category!]!
+    category(id: ID!): Category
   }
   type Product {
+    id: ID!
     name: String!
     description: String!
     quantity: Int!
@@ -98,16 +116,25 @@ const typeDefs = gql`
     price: Float!
     onSale: Boolean!
   }
+  type Category {
+    id: ID!
+    name: String!
+  }
 `
+
 const resolvers = {
   Query: {
     hello: () => 'World!!!',
     products: () => products,
     product: (parent, args, context) => {
+      const { id } = args;
       const productId = args.id;
-      const product = products.find(product => product.id === productId)
-      if (!product) return null;
-      return product;
+      return products.find(product => product.id === id);
+    },
+    categories: () => categories,
+    category: (parent, args, context) => {
+      const { id } = args
+      return categories.find((category) => category.id === id);
     }
   }
 }
@@ -118,5 +145,5 @@ const server = new ApolloServer({
 });
 
 server.listen().then(({ url }) => {
-  console.log('Server is ready at ' + url)
-})
+  console.log('Server is ready at ' + url);
+});
