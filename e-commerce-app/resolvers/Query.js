792d89fe373d1b9674cc59e09343f49f6f3d2101
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-05-26 14:20:58 +0530

    filter by average rating

--- e-commerce-app/resolvers/Query.js
+++ e-commerce-app/resolvers/Query.js
@@ -1,13 +1,28 @@
-const { products, categories } = require('../db');
+const { products, categories, reviews } = require('../db');
 
 exports.Query = {
   hello: (parent, args, { products }) => 'World!!!',
   products: (parent, { filter }, { products }) => {
     let filteredProducts = products;
     if(filter) {
-      if(filter.onSale === true) {
+      const { onSale, avgRating } = filter
+      if(onSale) {
         filteredProducts = filteredProducts.filter(product => {
           return product.onSale;
+        });
+      }
+      if([1,2,3,4,5].includes(avgRating)) {
+        filteredProducts = filteredProducts.filter((product) => {
+          let sumRating = 0;
+          let numberOfReviews = 0;
+          reviews.forEach((review) => {
+            if(review.productId === product.id) {
+              sumRating += review.rating
+              numberOfReviews++
+            }
+          });
+          const avgProductRating = sumRating/numberOfReviews
+          return avgProductRating >= avgRating
         })
       }
     }
