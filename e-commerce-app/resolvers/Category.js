cddd571de852050c57a2c42c37c6d1749f58766c
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-05-26 12:21:28 +0530

    filtering & input type

--- e-commerce-app/resolvers/Category.js
+++ e-commerce-app/resolvers/Category.js
@@ -1,7 +1,16 @@
 const { products } = require('../db');
 
 exports.Category = {
-  products: ({ id: categoryId }, args, { products }) => {
-    return products.filter((product) => product.categoryId === categoryId)
+  products: ({ id: categoryId }, { filter }, { products }) => {
+    const categoryProducts = products.filter((product) => product.categoryId === categoryId);
+    let filteredCategoryProducts = categoryProducts;
+    if(filter) {
+      if(filter.onSale === true) {
+        filteredCategoryProducts = filteredCategoryProducts.filter(product => {
+          return product.onSale;
+        })
+      }
+    }
+    return filteredCategoryProducts;
   },
 }
--- e-commerce-app/resolvers/Query.js
+++ e-commerce-app/resolvers/Query.js
@@ -2,7 +2,17 @@ const { products, categories } = require('../db');
 
 exports.Query = {
   hello: (parent, args, { products }) => 'World!!!',
-  products: (parent, args, { products }) => products,
+  products: (parent, { filter }, { products }) => {
+    let filteredProducts = products;
+    if(filter) {
+      if(filter.onSale === true) {
+        filteredProducts = filteredProducts.filter(product => {
+          return product.onSale;
+        })
+      }
+    }
+    return filteredProducts
+  },
   product: (parent, { id }, { products }) => {
     return products.find(product => product.id === id);
   },
