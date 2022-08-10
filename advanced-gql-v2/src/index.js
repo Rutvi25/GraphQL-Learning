2e477817e6cfaa4796489b10966a4bb70a1a2688
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-08-10 10:15:18 +0530

    authentication & subscription

---
+++ advanced-gql-v2/src/index.js
@@ -0,0 +1,31 @@
+const { ApolloServer, AuthenticationError } = require('apollo-server');
+
+const typeDefs = require('./typedefs');
+const resolvers = require('./resolvers');
+const { createToken, getUserFromToken } = require('./auth');
+const db = require('./db');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context({ req, connection }) {
+    const context = { ...db };
+    if (connection) {
+      return { ...context, ...connection.context };
+    }
+    const token = req.headers.authorization;
+    const user = getUserFromToken(token);
+    return { ...db, user, createToken };
+  },
+  subscriptions: {
+    onConnect(params) {
+      const token = params.authToken;
+      const user = getUserFromToken(token);
+      return { user };
+    },
+  },
+});
+
+server.listen(4000).then(({ url, subscriptionsUrl }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
